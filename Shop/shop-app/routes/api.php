98e4897dac61ad43e7a07da90171c08fa6b9625e
Kirill <kirill.spainish.load.net@gmail.com>
2022-09-20 19:13:25 +0300

    Adding Api to get Categories and Products

--- Shop/shop-app/routes/api.php
+++ Shop/shop-app/routes/api.php
@@ -1,5 +1,9 @@
 <?php
 
+use App\Http\Resources\CategoryResourse;
+use App\Http\Resources\ProductResourse;
+use App\Models\Category;
+use App\Models\Product;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -16,4 +20,16 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+Route::get('products', function () {
+    return ProductResourse::collection(Product::all());
+});
+Route::get('products/{id}', function ($id) {
+    return new ProductResourse(Product::findOrFail($id));
+});
+Route::get('Category', function () {
+    return CategoryResourse::collection(Category::all());
+});
+Route::get('Category/{id}', function ($id) {
+    return new CategoryResourse(Category::findOrFail($id));
 });
