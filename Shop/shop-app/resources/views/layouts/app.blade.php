0ed3e46721c361df9a5e5478c7b85ec0eb637782
Kirill <kirill.spainish.load.net@gmail.com>
2022-09-20 19:12:08 +0300

    Adding Factories and seeders

--- Shop/shop-app/resources/views/layouts/app.blade.php
+++ Shop/shop-app/resources/views/layouts/app.blade.php
@@ -20,8 +20,11 @@
     <div id="app">
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
+                <a class="navbar-brand" href="{{ url('/products') }}">
+                    {{ config('Goods', 'Goods') }}
+                </a>
+                <a class="navbar-brand" href="{{ url('/about') }}">
+                    {{ config('About us', 'About us') }}
                 </a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
@@ -55,6 +58,9 @@
                                 </a>
 
                                 <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+                                    <a class="dropdown-item" href="{{ url('/cart') }}">
+                                        {{ __('My orders') }}
+                                    </a>
                                     <a class="dropdown-item" href="{{ route('logout') }}"
                                        onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
